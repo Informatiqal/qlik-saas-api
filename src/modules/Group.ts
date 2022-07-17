3aa06803a32110f789e20cc6882361b2cbbfff72
Stefan Stoichev <stefan.stoichev@gmail.com>
2022-07-17 19:17:45 +0100

    Groups endpoints
    closes #56

---
+++ src/modules/Group.ts
@@ -0,0 +1,53 @@
+import { QlikSaaSClient } from "qlik-rest-api";
+
+export interface IGroup {
+  id: string;
+  name: string;
+  idpid: string;
+  links: {
+    href: string;
+  };
+  status: "active" | "disabled";
+  tenantId: string;
+  createdAt: string;
+  assignedRoles: {
+    id: string;
+    name: string;
+    type: string;
+    level: "admin" | "user";
+    permissions: string[];
+  }[];
+  lastUpdatedAt: string;
+}
+
+export interface IClassGroup {
+  details: IGroup;
+  remove(): Promise<number>;
+}
+
+export class Group implements IClassGroup {
+  private id: string;
+  private saasClient: QlikSaaSClient;
+  details: IGroup;
+  constructor(saasClient: QlikSaaSClient, id: string, details?: IGroup) {
+    if (!id) throw new Error(`group.get: "id" parameter is required`);
+
+    this.id = id;
+    this.saasClient = saasClient;
+    if (details) this.details = details;
+  }
+
+  async init() {
+    if (!this.details) {
+      this.details = await this.saasClient
+        .Get(`groups/${this.id}`)
+        .then((res) => res.data as IGroup);
+    }
+  }
+
+  async remove() {
+    return await this.saasClient
+      .Delete(`groups/${this.id}`)
+      .then((res) => res.status);
+  }
+}
