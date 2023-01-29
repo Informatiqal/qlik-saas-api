3c9baac2e169d4923c29c9317bc85afe6d2c0331
Stefan Stoichev <stefan.stoichev@gmail.com>
2023-01-29 05:54:06 +0000

    app scripts implementation
    closes #108

---
+++ src/modules/AppScript.ts
@@ -0,0 +1,76 @@
+import { QlikSaaSClient } from "qlik-rest-api";
+import { IScriptMeta, IScriptMetaWithScript } from "./Apps.interfaces";
+
+export interface IClassAppScript {
+  details: IScriptMetaWithScript;
+  getScriptContent(): Promise<string>;
+  remove(): Promise<number>;
+  update(name: string): Promise<number>;
+}
+
+export class AppScript implements IClassAppScript {
+  private id: string;
+  private appId: string;
+  private saasClient: QlikSaaSClient;
+  details: IScriptMetaWithScript;
+  constructor(
+    saasClient: QlikSaaSClient,
+    versionId: string,
+    appId: string,
+    details?: IScriptMetaWithScript
+  ) {
+    if (!versionId) throw new Error(`"versionId" parameter is required`);
+
+    this.id = versionId;
+    this.appId = appId;
+    this.saasClient = saasClient;
+
+    if (details) this.details = details;
+  }
+
+  async init() {
+    if (!this.details) {
+      this.details = await this.saasClient
+        .Get<IScriptMetaWithScript>(
+          `apps/${this.appId}/scripts?filter=ScriptId eq "${this.id}"`
+        )
+        .then((res) => ({ ...(res.data as IScriptMeta), script: "" }));
+    }
+  }
+
+  async getScriptContent() {
+    this.details.script = await this.saasClient
+      .Get<string>(`apps/${this.appId}/scripts/${this.id}`)
+      .then((res) => res.data);
+
+    return this.details.script;
+  }
+
+  async remove() {
+    return await this.saasClient
+      .Delete(`apps/${this.appId}/scripts/${this.id}`)
+      .then((res) => res.status);
+  }
+
+  /**
+   * Not very clear from the documentation what can be done here
+   *
+   * The UI allows only the name of the version (`versionMessage` ) to be updated.
+   * And because of this, this method only updates the name as well
+   */
+  async update(name: string) {
+    const updateStatus = await this.saasClient
+      .Patch(`apps/${this.appId}/scripts/${this.id}`, [
+        {
+          op: "replace",
+          path: "/versionMessage",
+          value: name,
+        },
+      ])
+      .then((res) => res.status);
+
+    this.details.versionMessage = name;
+
+    return updateStatus;
+  }
+}
