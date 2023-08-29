4f0591221bf5652b5755d39da7810bdeaa87d2d1
Stefan Stoychev <stefan.stoichev@gmail.com>
2023-08-29 09:19:05 +0100

    and the rest of the files

--- src/modules/Extension.ts
+++ src/modules/Extension.ts
@@ -1,4 +1,5 @@
-import { QlikSaaSClient } from "qlik-rest-api";
+import { QlikFormData, QlikSaaSClient } from "qlik-rest-api";
+import { IExtensionImportData } from "./Extensions";
 
 export interface IExtension {
   id: string;
@@ -46,8 +47,12 @@ export class Extension {
     this.saasClient = saasClient;
   }
 
-  async init() {
-    if (!this.details || Object.keys(this.details).length == 0) {
+  async init(arg?: { force: boolean }) {
+    if (
+      !this.details ||
+      Object.keys(this.details).length == 0 ||
+      arg?.force == true
+    ) {
       this.details = await this.saasClient
         .Get<IExtension>(`extensions/${this.id}`)
         .then((res) => res.data);
@@ -74,5 +79,66 @@ export class Extension {
         "arraybuffer"
       )
       .then((res) => res.data);
+  }
+
+  /**
+   * **WARNING!** 
+   * 
+   * At this point (2023-08-29) not all "data" properties can be updated.
+   * Qlik will reply with status 200 ("OK. Extension has been updated.") but the extension
+   * is not updated. For example: if we pass new value for "name" property the update request
+   * will be ok but the extension name is not updated in Qlik.
+   *
+   * Although the documentation states that all properties can be passed that is not
+   * reflecting the reality. From my experience the only things that can be updates
+   * at the moment are the "file" and "data.tags"
+   *
+   * This method will anyway accept all "data" properties and when one day Qlik enable
+   * all properties on their end then this method will not require any updating
+   */
+  async update(
+    arg:
+      | {
+          file: string | Buffer;
+          data: Partial<IExtensionImportData>;
+        }
+      | {
+          file?: undefined;
+          data: Partial<IExtensionImportData>;
+        }
+      | {
+          file: string | Buffer;
+          data?: undefined;
+        }
+  ) {
+    if (!arg) throw new Error(`extensions.update: no arguments were provided`);
+    if (!arg.file && !arg.data)
+      throw new Error(
+        `extensions.update: "file" and "data" arguments are missing`
+      );
+
+    const fd = new QlikFormData();
+
+    if (arg.file)
+      fd.append({
+        field: "file",
+        data: arg.file,
+        contentType: "application/x-zip-compressed",
+      });
+
+    if (arg.data)
+      fd.append({
+        field: "data",
+        data: JSON.stringify(arg.data),
+      });
+
+    let updateStatus = 0;
+    return await this.saasClient
+      .Patch<IExtension>(`extensions/${this.id}`, fd.getData, fd.getHeaders)
+      .then((res) => {
+        updateStatus = res.status;
+        return this.init({ force: true });
+      })
+      .then(() => updateStatus);
   }
 }
--- src/modules/Extensions.ts
+++ src/modules/Extensions.ts
@@ -1,6 +1,113 @@
-import { QlikSaaSClient } from "qlik-rest-api";
+import { QlikFormData, QlikSaaSClient } from "qlik-rest-api";
 import { Extension, IExtension } from "./Extension";
 import { parseFilter } from "../util/filter";
+
+export interface IExtensionImportData {
+  /**
+   * The display name of this extension.
+   */
+  name: string;
+  /**
+   * The type of this extension (visualization, etc.).
+   */
+  type: string;
+  /**
+   * Icon to show in the client.
+   */
+  icon?: string;
+  /**
+   * List of tags.
+   */
+  tags?: string[];
+  /**
+   * Author of the extension.
+   */
+  author?: string;
+  /**
+   * Object containing meta data regarding the bundle the extension belongs to.
+   * If it does not belong to a bundle, this object is not defined.
+   */
+  bundle?: {
+    /**
+     * Unique identifier of the bundle.
+     */
+    id: string;
+    /**
+     * Name of the bundle.
+     */
+    name: string;
+    /**
+     * Description of the bundle.
+     */
+    description?: string;
+  };
+  userId?: string;
+  /**
+   * If the extension is part of an extension bundle.
+   */
+  bundled?: boolean;
+  /**
+   * Under which license this extension is published.
+   */
+  license?: string;
+  /**
+   * An image that enables users to preview the extension.
+   */
+  preview?: string;
+  /**
+   * Version of the extension.
+   */
+  version?: string;
+  /**
+   * Checksum of the extension contents.
+   */
+  checksum?: string;
+  /**
+   * Home page of the extension.
+   */
+  homepage?: string;
+  /**
+   * Keywords for the extension.
+   */
+  keywords?: string;
+  /**
+   * Relative path to the extension's entry file, defaults to filename from the qext file.
+   */
+  loadpath?: string;
+  /**
+   * Supplier of the extension.
+   */
+  supplier?: string;
+  tenantId?: string;
+  /**
+   * If the extension is a supernova extension or not.
+   */
+  supernova?: boolean;
+  /**
+   * A date noting when the extension was deprecated.
+   */
+  deprecated?: string;
+  /**
+   * Link to the extension source code.
+   */
+  repository?: string;
+  /**
+   * Description of the extension.
+   */
+  description?: string;
+  /**
+   * The version from the qext file that was uploaded with this extension.
+   */
+  qextVersion?: string;
+  /**
+   * Map of dependencies describing version of the component it requires.
+   */
+  dependencies?: {};
+  /**
+   * The name of the qext file that was uploaded with this extension.
+   */
+  qextFilename?: string;
+}
 
 //TODO: import extension method
 export class Extensions {
@@ -51,5 +158,28 @@ export class Extensions {
         )
       )
     );
+  }
+
+  async import(arg: { file: string | Buffer; data: IExtensionImportData }) {
+    if (!arg) throw new Error(`extensions.import: no arguments were provided`);
+    if (!arg.file)
+      throw new Error(`extensions.import: "file" argument is missing`);
+
+    const fd = new QlikFormData();
+    
+    fd.append({
+      field: "file",
+      data: arg.file,
+      contentType: "application/x-zip-compressed",
+    });
+
+    fd.append({
+      field: "data",
+      data: JSON.stringify(arg.data),
+    });
+
+    return await this.saasClient
+      .Post<IExtension>("extensions", fd.getData, fd.getHeaders)
+      .then((res) => new Extension(this.saasClient, res.data.id, res.data));
   }
 }
